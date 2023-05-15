be00d71176b22ee29d950be881a85ef440eaec68
ryu-jaehyun <vo6182@gmail.com>
2023-05-16 00:08:41 +0900

    commit

--- src/main/java/com/example/demo/user/LoginRequest.java
+++ src/main/java/com/example/demo/user/LoginRequest.java
@@ -5,6 +5,11 @@ public class LoginRequest {
     private String userId;
     private String userPw;
 
+    public LoginRequest(String userId, String userPw) {
+        this.userId = userId;
+        this.userPw = userPw;
+    }
+
     public String getUserId() {
         return userId;
     }
