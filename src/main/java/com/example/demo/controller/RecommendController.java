0e0f5d6700f9dec63a4669c05639509ba3da844e
ryu-jaehyun <vo6182@gmail.com>
2023-05-28 21:40:39 +0900

    commit

--- src/main/java/com/example/demo/controller/RecommendController.java
+++ src/main/java/com/example/demo/controller/RecommendController.java
@@ -2,29 +2,30 @@ package com.example.demo.controller;
 
 import com.example.demo.image.ImageDocument;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/images")
-public class ImageController {
+@RequestMapping("/api")
+public class RecommendController {
     @Autowired
     private final MongoTemplate mongoTemplate;
 
-    @Autowired
-    public ImageController(MongoTemplate mongoTemplate) {
+    public RecommendController(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
     }
     @CrossOrigin(origins = "*")
-    @GetMapping
-    public List<ImageDocument> getAllReviews() {
-        List<ImageDocument> reviews = mongoTemplate.findAll(ImageDocument.class, "Restaurant_Review");
-        return reviews;
+    @GetMapping("/recommend")
+    public List<ImageDocument> getTopRatedReviews() {
+        Query query = new Query().with(Sort.by(Sort.Direction.DESC, "rate")).limit(4);
+        List<ImageDocument> topRatedReviews = mongoTemplate.find(query, ImageDocument.class, "Restaurant_Review");
+        return topRatedReviews;
     }
 }
