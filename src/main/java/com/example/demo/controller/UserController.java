81705379a45c72d822a19c6e567ac5f9f362e687
ryu-jaehyun <vo6182@gmail.com>
2023-05-30 02:15:47 +0900

    commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.user.*;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,12 +33,31 @@ public class UserController {
     }
    @CrossOrigin(origins = "*")
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody User user1) {
+    public ResponseEntity<Object> login(@RequestBody User user1,  HttpServletRequest request,
+                                        HttpServletResponse response) {
         String userId = user1.getId();
         String userPw = user1.getPassword();
-
+       System.out.println(userId);
         User user = userRepository.findByUserIdAndPassword(userId, userPw);
         if(user !=null){
+            // 기존 세션 무효화 -->로그아웃 기능 간단하게 구현
+            HttpSession session = request.getSession(false);
+            if (session != null) {
+                session.invalidate();
+            }
+
+            // 새로운 세션 생성
+            HttpSession newSession = request.getSession(true);
+
+            newSession.setAttribute("userId", user.getId());
+            Object a =newSession.getId();
+            System.out.println(a);
+
+            // 쿠키 생성
+            Cookie sessionCookie = new Cookie("sessionId", newSession.getId());
+            sessionCookie.setMaxAge(60*60*24 ); // 쿠키 유효 기간 설정 (예: 1일)
+            sessionCookie.setPath("/"); // 쿠키의 유효 경로 설정
+            response.addCookie(sessionCookie);
             return ResponseEntity.ok("Login successful!");
         }
         else{
