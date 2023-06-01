5ee84e5d9ec53e274ced089a48b86b7f09c9e264
ryu-jaehyun <vo6182@gmail.com>
2023-06-02 01:56:06 +0900

    commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -53,31 +53,24 @@ public class UserController {
     }
    @CrossOrigin(origins = "*")
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody User user1,  HttpServletRequest request,
-                                        HttpServletResponse response) {
+    public ResponseEntity<Object> login(@RequestBody User user1,  HttpServletRequest request
+                                        ) {
         String userId = user1.getId();
         String userPw = user1.getUserPw();
        System.out.println(userId);
         User user = userRepository.findByUserIdAndPassword(userId, userPw);
         if(user !=null){
-            // 기존 세션 무효화 -->로그아웃 기능 간단하게 구현
-            HttpSession session = request.getSession(false);
-            if (session != null) {
-                session.invalidate();
-            }
+
 
             // 새로운 세션 생성
             HttpSession newSession = request.getSession(true);
 
             newSession.setAttribute("userId", user.getId());
+            newSession.setMaxInactiveInterval(1800);
             Object a =newSession.getId();
             System.out.println(a);
 
-            // 쿠키 생성
-            Cookie sessionCookie = new Cookie("sessionId", newSession.getId());
-            sessionCookie.setMaxAge(60*60*24 ); // 쿠키 유효 기간 설정 (예: 1일)
-            sessionCookie.setPath("/"); // 쿠키의 유효 경로 설정
-            response.addCookie(sessionCookie);
+
             return ResponseEntity.ok("Login successful!");
         }
         else{
