e7f6010da0cca7bd4292dd1bff467ed0bf8c09ee
ryu-jaehyun <vo6182@gmail.com>
2023-06-03 03:58:38 +0900

    commit

--- src/main/java/com/example/demo/user/WebConfig.java
+++ src/main/java/com/example/demo/user/WebConfig.java
@@ -7,10 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // vite 쓰는 리액트에서 오리진 요청을 열어줘야함 (포트 주의)
-                .allowedMethods("*") // http 모든 메소드 요청 허용
-                .allowedHeaders("*") // 헤더 정보 모두 허용
-                .allowCredentials(true); // 쿠키, 세션 정보도 허용
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
     }
 }
